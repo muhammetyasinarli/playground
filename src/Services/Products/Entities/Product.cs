be15cf7079a3e16c8fdf2d38a144e60003cfd8c0
Muhammet Yasin ARLI <MuhammetYasin.Arli@logo.com.tr>
2021-08-11 14:31:21 +0300

    refactoring and new migration for price column

--- src/Services/Products/Entities/Product.cs
+++ src/Services/Products/Entities/Product.cs
@@ -1,6 +1,7 @@
 ﻿using Products.Entities.Base;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -13,6 +14,7 @@ namespace Products.Entities
         public string Summary { get; set; }
         public string Description { get; set; }
         public string ImageFile { get; set; }
+        [Column(TypeName = "decimal(18,2)")]
         public decimal Price { get; set; }
     }
 }
